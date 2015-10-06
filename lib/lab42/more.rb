960ca5dea65e34898cc57fe50217bfdaa6e8623b
RobertDober <robert.dober@gmail.com>
2015-10-06 22:14:44 +0200

    VERSION 0.0 created

--- lib/lab42/more.rb
+++
@@ -1,6 +0,0 @@
-require_relative 'more/enumerable_methods'
-require_relative 'more/behavior'
-require_relative 'more/behavior/predefined'
-require_relative 'more/kernel/behavior'
-
-require_relative 'more/functional_emancipation'
